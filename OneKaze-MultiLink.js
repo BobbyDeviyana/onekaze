dd954070883e8efefffc516443378e61f485a13a
BobbyDeviyana <33723288+BobbyDeviyana@users.noreply.github.com>
2017-11-18 09:49:50 +0700

    Update OneKaze-MultiLink.js

--- OneKaze-MultiLink.js
+++ OneKaze-MultiLink.js
@@ -19,13 +19,12 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="what-are-risk-factors-for-malignant.html"}
 if(x=="2"){xxx="pericardial-mesothelioma-treatment.html"}
 if(x=="3"){xxx="4-steps-to-relieve-back-pain.html"}
-if(x=="4"){xxx="7-guidelines-to-choosing-great-patio.html"}
-if(x=="5"){xxx="5-ways-to-manage-your-diet-for-diabetes.html"}a_to_ve[i].href="https://onekaze.blogspot.com/2017/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="https://onekaze.blogspot.com/2017/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
